f50467e2b9d1387c2d6722327b4063c6bc233a19
Sai Kumar Nagumantri <saikumar1993.ece@gmail.com>
2026-04-19 17:45:42 +0530

    Implemented Logger and error Handling

--- apps/products/src/products/products.controller.ts
+++ apps/products/src/products/products.controller.ts
@@ -1,14 +1,16 @@
 import { ApiOkResponse, ApiTags } from "@nestjs/swagger";
 import { ProductDTO } from "./dto/product.dto";
 import { ProductsService } from "./products.service";
-import { Controller, Get, Param } from "@nestjs/common";
+import { BadRequestException, Controller, Get, Logger, Param } from "@nestjs/common";
 
 @ApiTags('products')
 @Controller('products')
 export class ProductsController {
 
-  constructor(private readonly productService: ProductsService) {}
-
+  constructor(private readonly productService: ProductsService) {
+    
+  }
+private readonly logger = new Logger(ProductsController.name)
   @Get()
   @ApiOkResponse({ type: ProductDTO, isArray: true })
   getProducts(): ProductDTO[] {
@@ -17,7 +19,13 @@ export class ProductsController {
 
   @Get(':id')
   @ApiOkResponse({ type: ProductDTO })
-  getProductById(@Param('id') id: string): ProductDTO | undefined {
-    return this.productService.getProductById(Number(id));
+  getProductById(@Param('id') id: number): ProductDTO | undefined {
+      const productId = Number(id);
+
+    if(isNaN(productId)){
+      this.logger.warn(`Invalid product id: ${id}`)
+      throw new BadRequestException(`Invalid product id: ${id}`)
+    }
+    return this.productService.getProductById(Number(productId));
   }
 }
--- apps/products/src/products/products.service.ts
+++ apps/products/src/products/products.service.ts
@@ -1,13 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { PRODUCTS } from './data/products.data';
 import { ProductDTO } from './dto/product.dto';
 
 @Injectable()
 export class ProductsService {
-  getProducts():ProductDTO[] {
-    return PRODUCTS
+  private readonly logger = new Logger(ProductsService.name);
+  getProducts(): ProductDTO[] {
+    return PRODUCTS;
   }
-  getProductById(id: number):ProductDTO | undefined {
-    return  PRODUCTS.find(p => p.id === +id);
+  getProductById(id: number): ProductDTO | undefined {
+    const product = PRODUCTS.find((p) => p.id === +id);
+    if (!product) {
+      // 2. LOG the error for internal tracking (Developer view)
+      this.logger.warn(`Search failed: Product with ID ${id} not found.`);
+
+      // 3. THROW the exception for the API response (Client view)
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+    this.logger.log(`Successfully fetched product: ${product.name}`);
+    return product;
   }
 }
